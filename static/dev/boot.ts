e1fecad0cdb48dd3532acf56a8dd50ba7af01720
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-05-07 23:36:53 +0300

    Added reference to browser.d.ts which was causing some errors

--- static/dev/boot.ts
+++ static/dev/boot.ts
@@ -1,6 +1,9 @@
+/// <reference path="../../node_modules/angular2/typings/browser.d.ts" />
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
 import {ROUTER_PROVIDERS} from "angular2/router";
 import {HTTP_PROVIDERS} from "angular2/http";
+// Add all operators to Observable
+import 'rxjs/Rx'
 
 bootstrap(AppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
