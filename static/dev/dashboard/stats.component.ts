f1e57ddf5040fcb42c990e32587b90c1379f0c99
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-05-07 23:37:14 +0300

    Added some divs, which are yet to be styled

--- static/dev/dashboard/stats.component.ts
+++ static/dev/dashboard/stats.component.ts
@@ -3,9 +3,16 @@ import {Component} from "angular2/core";
     selector: 'dashboard-stats',
     template: `
         <h3>Stats</h3>
-        <p>
-            System stats will come here i.e. three bubbles
-        </p>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+                <div class="databox bg-white">
+
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6"></div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6"></div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6"></div>
+        </div>
     `
 })
 
