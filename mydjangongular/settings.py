2a8fa041c186d52f43fd6eac3970816c191e11bf
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-05-13 18:37:32 +0300

    Added authentication settings

--- mydjangongular/settings.py
+++ mydjangongular/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_jwt',
     'corsheaders',
     'mydjangongular.modules.app',
     'mydjangongular.modules.api'
@@ -132,7 +133,15 @@ STATICFILES_DIRS = [
 # Django Rest Framework
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
 }
 
 # cors
